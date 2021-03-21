41964592861e30151398bdabb55d679edad592eb
Connor <connor.berechree@my.jcu.edu.au>
2021-03-21 16:16:35 +1000

    Broken score and temperatures refactored.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,11 +1,25 @@
-score = float(input("Enter score: "))
-if score < 50:
-    print("Bad")
-elif score > 100:
-    print("Invalid score")
-elif score > 50:
-    print("Passable")
-elif score > 90:
-    print("Excellent")
-else:
-    print("Invalid Score")
+def main():
+
+    score = ask_score()
+    score_decision(score)
+
+
+def ask_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+def score_decision(score):
+    if score < 50:
+        print("Bad")
+    elif score > 100:
+        print("Invalid score")
+    elif score > 50:
+        print("Passable")
+    elif score > 90:
+        print("Excellent")
+    else:
+        print("Invalid Score")
+
+
+main()
