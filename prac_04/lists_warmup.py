36b33e977a4038b4abc272649858f6109d45d3c7
Connor <connor.berechree@my.jcu.edu.au>
2021-03-30 12:43:46 +1000

    Add work, prac 4

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,15 @@
+#1. 3
+#2. 2
+#3. 1
+#4. all of numbers
+#5. 1
+#6. numbers[4]
+#7. numbers + [7], numbers[-1]
+#8. numbers[0]
+#9. numbers = [3, 1, 4, 1, 5, 9, 6, 5, 3]
+
+# python expressions
+#1.
+#2.
+#3.
+#4.
